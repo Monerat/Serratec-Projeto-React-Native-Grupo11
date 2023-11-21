d923e9aa03b9e5e1a630b325db935af32621da28
IsaquePerezRodrigues <isaquenovo127@gmail.com>
2023-11-21 17:39:06 -0300

    add: component barType

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -9,27 +9,9 @@ import { useFonts } from 'expo-font';
 import AppLoading from 'expo-app-loading'
 import { FontAwesome5 } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
-import elementFire from '../../assets/images/ElementFire.png'
-import elementWater from '../../assets/images/ElementWater.png'
-import elementBug from '../../assets/images/ElementBug.png'
-import elementDark from '../../assets/images/ElementDark.png'
-import elementDragon from '../../assets/images/ElementDragon.png'
-import elementEletric from '../../assets/images/ElementEletric.png'
-import elementFairy from '../../assets/images/ElementFary.png'
-import elementFight from '../../assets/images/ElementFight.png'
-import elementFly from '../../assets/images/ElementFlying.png'
-import elementGhost from '../../assets/images/ElementGhost.png'
-import elementGrass from '../../assets/images/ElementGrass.png'
-import elementGround from '../../assets/images/ElementGround.png'
-import elementIce from '../../assets/images/ElementIce.png'
-import elementSteel from '../../assets/images/ElementMetal.png'
-import elementNormal from '../../assets/images/ElementNormal.png'
-import elementPoison from '../../assets/images/ElementPoison.png'
-import elementPsychic from '../../assets/images/ElementPsychic.png'
-import elementRock from '../../assets/images/ElementRock.png'
-import elementToxic from '../../assets/images/ElementToxic.png'
 import { LinearGradient } from 'expo-linear-gradient';
 import { transparent } from 'react-native-paper/lib/typescript/styles/themes/v2/colors';
+import { barraType } from '../BarType';
 
 interface PokemonCardProps {
   item: Pokemon;
@@ -88,54 +70,12 @@ export const PokemonCard = ({ item }: PokemonCardProps) => {
       default:
         return "#ffffff";
     }
-  };   
+  };     
   
-  const barraType = (type?: string) => {
-    switch (type) {
-      case "normal":
-        return elementNormal;
-      case "fire":
-        return elementFire;
-      case "water":
-        return elementWater;
-      case "grass":
-        return elementGrass;
-      case "flying":
-        return elementFly;
-      case "fighting":
-        return elementFight;
-      case "poison":
-        return elementPoison;
-      case "electric":
-        return elementEletric;
-      case "ground":
-        return elementWater;
-      case "rock":
-        return elementRock;
-      case "psychic":
-        return elementPsychic;
-      case "ice":
-        return elementIce;
-      case "bug":
-        return elementBug;
-      case "ghost":
-        return elementGhost;
-      case "steel":
-        return elementSteel;
-      case "dragon":
-        return elementDragon;
-      case "dark":
-        return elementDark;
-      case "fairy":
-        return elementFairy;
-      default:
-        return elementNormal
-    }
-  };
   const styleDynamic = Math.random() < 0.5 ? styles.container : styles.prismaticColor
   return (
     <View style={[styleDynamic, { backgroundColor: corDoCardPeloType(types[0].type.name) }]}>
-      <LinearGradient colors={["#2790F0", "#9D53B2","transparent"]}>
+      {/* <LinearGradient colors={["#2790F0", "#9D53B2","transparent"]}> */}
       <View style={styles.header}>
         <Text style={[styles.textPokemonName, {fontFamily: 'gillBold'}]}>{name}</Text>
         <View style={styles.containerHp}>
@@ -194,8 +134,8 @@ export const PokemonCard = ({ item }: PokemonCardProps) => {
       </View>
       <View style={styles.containerType}>
         {/* <Text style={[styles.textPokemonType, {fontFamily: 'gillBold'}]}>{types[0].type.name}</Text> */}
-        <Image style={{width:"100%", height:"100%"}} source={barraType(types[0].type.name)} />
+        <Image style={{width:"100%", height:"100%", marginBottom: "8%"}} source={barraType(types[0].type.name)} />
       </View>
-      </LinearGradient>
+      {/* </LinearGradient> */}
     </View>
   )}
